f361843035e30528a7a4bf7dc994e3794ca76763
Andrew Walker <a.walker@leeds.ac.uk>
2017-01-06 09:52:02 +0000

    Stuff adter I've finished teaching this.

--- fib.py
+++ fib.py
@@ -2,4 +2,4 @@ def fib(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return fib(n - 1) + fib(n - 1)
+        return fib(n - 1) + fib(n - 2)
---
+++ files_for_travisCI/README.md
@@ -0,0 +1,3 @@
+This directory includes two file you need to turn on travisCI 
+for automated testing. One is hidden (.travis.yaml).
+
